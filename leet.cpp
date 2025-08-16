a54e86e19ba91ae8f7826215df37314030332443
6ankit <ankitlekharajani752@gmail.com>
2025-08-16 13:49:42 +0530

    Added the POTD of 16 august 2025

--- leet.cpp
+++ leet.cpp
@@ -1,5 +1,34 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"Git and gitHub sikh rhe hai";
+    int num;
+    cin>>num;  //Dont take input other than the combination of 9 and 6
+    string s="";
+    s+=to_string(num);
+    int maxi=num;
+    for(int i=0;i<s.size();i++)
+    {
+      if(s[i]=='6')
+      {
+        s[i]='9';
+        int temp=stoi(s);
+        maxi=max(maxi,temp);
+        s[i]='6';
+      }
+    }
+    s="";
+    s+=to_string(num);
+    int maxi2=num;
+    for(int i=0;i<s.size();i++)
+    {
+      if(s[i]=='9')
+      {
+        s[i]='6';
+        int temp=stoi(s);
+        maxi=max(maxi,temp);
+        s[i]='9';
+      }
+    }
+    cout<<max(maxi,maxi2);
 }
